60b0c3cd5a97db2e158ca2dd52e7c1c006890f9f
Lukasz Bukowski <luk3@interia.pl>
2019-05-14 00:43:49 +0200

    Added js scroll transformation for site-header and overlay for gallery images

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,3 +8,25 @@ menuIcon.addEventListener("click", function() {
   menuContent.classList.toggle("site-header__menu-content--is-visible");
   menuIcon.classList.toggle("site-header__menu-icon--x-close");
 });
+
+//gallery images overlay
+const galleryItems = document.querySelectorAll(".gallery__item");
+
+galleryItems.forEach(function(item) {
+  item.addEventListener("mouseover", function() {
+    item.classList.add("is-active");
+  });
+  item.addEventListener("mouseout", function() {
+    item.classList.remove("is-active");
+  });
+});
+
+//reduce site-header size ofter scrolling
+window.addEventListener("scroll", function() {
+  const scrollPosition = window.scrollY;
+  if (scrollPosition > 100) {
+    siteHeader.classList.add("site-header--darker");
+  } else if (siteHeader.classList.contains("site-header--darker")) {
+    siteHeader.classList.remove("site-header--darker");
+  }
+});
