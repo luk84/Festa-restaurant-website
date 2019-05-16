68e1a05b2c5ba43f7f15d3589bdc9a20a21a7177
Lukasz Bukowski <luk3@interia.pl>
2019-05-16 18:45:35 +0200

    Added hero image animation with img preloading and fixed some margins

--- dist/js/main.js
+++ dist/js/main.js
@@ -43,3 +43,12 @@ window.addEventListener("scroll", function() {
     siteHeader.classList.remove("site-header--darker");
   }
 });
+
+//animations delay
+document.body.classList.add("js-loading");
+
+window.addEventListener("load", showPage);
+
+function showPage() {
+  document.body.classList.remove("js-loading");
+}
